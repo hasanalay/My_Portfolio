61c51bdc1e0671d0eb82c51aefd99ddb1373b223
hasanalay <hsn_aly@hotmail.com>
2023-07-26 20:00:04 +0300

    update vercel settings

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
 echo "Build Started"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.11 -m pip install -r requirements.txt
+python3.11 manage.py collectstatic --noinput --clear
