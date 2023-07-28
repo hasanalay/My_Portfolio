4239bcfca83ea525932a8b4376b1607a26e4114e
hasanalay <hsn_aly@hotmail.com>
2023-07-28 19:34:21 +0300

    edit repo for deploy to vercel

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,3 @@
 echo "Build Started"
-python3.9 -m pip install -r requirements.txt
-python3.9 -m pip install db-sqlite3
+pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
