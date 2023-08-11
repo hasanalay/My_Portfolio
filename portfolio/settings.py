d237643795a1a737ddde49dfb6164f86060f9a74
Hasan ALAY <hsn_aly@hotmail.com>
2023-08-11 23:56:48 +0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '/var/task/django/contrib/admin/templates/')],
+        'DIRS': [os.path.join(BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
