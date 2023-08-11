ca477cca4b0882f82fd8c2876b0b8428d2cc364f
hasanalay <hsn_aly@hotmail.com>
2023-08-11 22:33:20 +0300

    edit the build file

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
-python3.9 manage.py createsuperuser --noinput --username=$DJANGO_SUPERUSER_USERNAME --email=$DJANGO_SUPERUSER_EMAIL --password=$DJANGO_SUPERUSER_PASSWORD
 pip install -r requirements.txt
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python3 manage.py shell
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic --noinput --clear
 
