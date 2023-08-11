97ec0be92f0a16bfbad2b7e36ad29b178e53d747
hasanalay <hsn_aly@hotmail.com>
2023-08-11 22:21:55 +0300

    edit file

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,5 @@
-
+python3.9 manage.py createsuperuser --noinput --username=$DJANGO_SUPERUSER_USERNAME --email=$DJANGO_SUPERUSER_EMAIL --password=$DJANGO_SUPERUSER_PASSWORD
 pip install -r requirements.txt
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic --noinput --clear
-python manage.py createsuperuser --noinput \
-    --username=$DJANGO_SUPERUSER_USERNAME \
-    --email=$DJANGO_SUPERUSER_EMAIL \
-    --password=$DJANGO_SUPERUSER_PASSWORD
+
