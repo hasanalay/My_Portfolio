7ab471f8167da9491b637bf9e874bfb4ce36630b
hasanalay <hsn_aly@hotmail.com>
2023-08-11 23:01:53 +0300

    fix mmigration

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 pip install -r requirements.txt
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python3 manage.py shell
+python3.9 manage.py makemigrations
 python3.9 manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python3.9 manage.py shell
 python3.9 manage.py collectstatic --noinput --clear
 
