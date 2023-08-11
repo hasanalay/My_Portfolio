85f074c36be2e15df5280c702310f53bf2ebc77b
hasanalay <hsn_aly@hotmail.com>
2023-08-11 22:20:02 +0300

    add django admin props

--- build_files.sh
+++ build_files.sh
@@ -2,3 +2,7 @@
 pip install -r requirements.txt
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic --noinput --clear
+python manage.py createsuperuser --noinput \
+    --username=$DJANGO_SUPERUSER_USERNAME \
+    --email=$DJANGO_SUPERUSER_EMAIL \
+    --password=$DJANGO_SUPERUSER_PASSWORD
